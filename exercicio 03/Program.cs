3c57710a2dddeb8b84742dbc5cd76698515e76bb
Desenvolvedor <Desenvolvedor@P2200042300>
2023-09-25 18:07:36 -0300

    Add project files.

---
+++ exercicio 03/Program.cs
@@ -0,0 +1,36 @@
+﻿int salarioBruto, valorHora, horasTrabalhadas, salarioLiquido, ir,  descontos;
+
+Console.WriteLine("Digite o valor da hora trabalhada: ");
+valorHora = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite a quantidade de horas trabalhadas: ");
+horasTrabalhadas = Convert.ToInt32(Console.ReadLine());
+
+salarioBruto = valorHora * horasTrabalhadas;
+
+if (salarioBruto > 900 && < 1501)
+{
+    descontos = (int)(salarioBruto * 0.05);
+    salarioLiquido = salarioBruto - descontos;
+    ir = 5;
+}
+else
+{
+    if(salarioBruto <= 2500)
+    {
+        descontos = (int)(salarioBruto * 0.10);
+        salarioLiquido = salarioBruto - descontos;
+        ir = 10;
+    }
+   
+    if(salarioBruto >2500)
+    {
+        descontos = (int)(salarioBruto * 0.20);
+        salarioLiquido = salarioBruto - descontos;
+        ir = 20;
+    }
+    
+}
+
+Console.WriteLine("Salário bruto (" + valorHora + "x" + horasTrabalhadas ")");
+Console.WriteLine("IR")
---
+++ exercicio 2/Program.cs
@@ -0,0 +1,124 @@
+﻿int numero1, numero2, numero3, menorNumero = 0, maiorNumero = 0, numeroMeio = 0;
+
+Console.WriteLine("Digite o primeiro número: ");
+numero1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número: ");
+numero2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o terceiro número: ");
+numero3 = Convert.ToInt32(Console.ReadLine());
+
+
+if (numero1 > numero2)
+{
+    if (numero1 > numero3)
+    {
+        maiorNumero = numero1;
+        
+        if(numero2 > numero3)
+        {
+            numeroMeio = numero2;
+        }
+        else
+        {
+            numeroMeio = numero3;
+        }
+    }
+    else
+    {
+        maiorNumero = numero3;
+    }
+
+
+}
+
+if (numero2 > numero3)
+{
+    if (numero2 > numero1)
+    {
+        maiorNumero = numero2;
+
+        if (numero3 > numero1)
+        {
+            numeroMeio = numero3;
+        }
+        else
+        {
+            numeroMeio = numero1;
+        }
+    }
+    else
+    {
+        maiorNumero = numero1;
+    }
+
+
+}
+
+if (numero3 > numero2)
+{
+    if (numero3 > numero1)
+    {
+        maiorNumero = numero3;
+
+        if (numero2 > numero1)
+        {
+            numeroMeio = numero2;
+        }
+        else
+        {
+            numeroMeio = numero1;
+        }
+    }
+    else
+    {
+        maiorNumero = numero1;
+    }
+
+
+}
+
+//a
+
+if (numero1 < numero2)
+{
+    if (numero1 < numero3)
+    {
+        menorNumero = numero1;
+    }
+    else
+    {
+        menorNumero = numero3;
+    }
+
+
+}
+
+
+if (numero2 < numero3)
+{
+    if (numero2 < numero1)
+    {
+        menorNumero = numero2;
+    }
+    else
+    {
+        menorNumero = numero1;
+    }
+}
+
+if (numero3 < numero2)
+{
+    if (numero3 < numero1)
+    {
+        menorNumero = numero3;
+    }
+    else
+    {
+        menorNumero = numero1;
+    }
+}
+Console.WriteLine("O maior número é: " + maiorNumero);
+Console.WriteLine("O número do meio é:" + numeroMeio);
+Console.WriteLine("O menor número é: " + menorNumero);
